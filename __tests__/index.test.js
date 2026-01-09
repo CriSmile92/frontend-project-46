12f8d56c65ef47259eea235d0d6012d0bb8019d3
CriSmile92 <crismile206@gmail.com>
2026-01-09 09:55:24 +0300

    refactoring index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,7 +7,7 @@ import genDiff from '../src/index.js'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 const readFixture = filename => fs.readFileSync(getFixturePath(filename), 'utf-8')
-// eslint-disable-next-line
+
 describe('genDiff', () => {
   test('JSON format', () => {
     expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json'))
